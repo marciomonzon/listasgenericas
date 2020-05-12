fab3f8634ccc711c45badd635746c192939c281e
Marcio <marcio.pcmonzon@gmail.com>
2020-05-12 14:23:16 -0300

    finalizando curso listas genericas

--- ManipulacaoListas/ManipulacaoListas/Program.cs
+++ ManipulacaoListas/ManipulacaoListas/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ManipulacaoListas
 {
@@ -6,7 +7,51 @@ namespace ManipulacaoListas
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<int> minhaLista = new List<int>();
+            List<int> minhaLista2 = new List<int>();
+
+            // adicionar ==> add
+            minhaLista.Add(1);
+            minhaLista.Add(2);
+            minhaLista.Add(3);
+            minhaLista.Add(4);
+
+            Console.WriteLine(minhaLista.Capacity);
+            Console.WriteLine(minhaLista.Count);
+
+            minhaLista2.Add(5);
+            minhaLista2.Add(5);
+            minhaLista2.Add(6);
+            minhaLista2.Add(7);
+            minhaLista2.Add(8);
+
+            foreach (int item in minhaLista)
+            {
+                Console.WriteLine(item);
+            }
+
+            // adiciona na ultima posição
+            minhaLista.AddRange(minhaLista2);
+
+            for (int i = 0; i < minhaLista.Count; i++)
+            {
+                Console.WriteLine(minhaLista[i]);
+            }
+
+            // ordena lista em ordem crescente
+            minhaLista.Sort();
+
+            minhaLista.Contains(8); // retorna true or false
+
+            minhaLista.Insert(2, 10); // insere em um determinado indice
+
+            minhaLista.InsertRange(3, minhaLista2); // insere uma collection em um determinado indice
+
+            minhaLista.Remove(8); // remove algo especifico informado
+
+            minhaLista.Clear(); // remove os elementos
+
+            Console.ReadKey();
         }
     }
 }
